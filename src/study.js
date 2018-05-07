283534235acfd74f864f1c35da2856dd456373b2
Jonathan Kingston <jonathan@jooped.co.uk>
2018-05-07 15:37:31 +0100

    Adding in placeholders into study page

--- src/study.js
+++ src/study.js
@@ -1,11 +1,53 @@
 async function init() {
+  // TODO speak with gandalf about new i18n lib to use fragments instead of this
   const elements = [...document.querySelectorAll("[data-i18n-message]")];
   for (const element of elements) {
-    // TODO speak with gandalf about new i18n lib to use fragments instead of this
     element.innerHTML = DOMPurify.sanitize(browser.i18n.getMessage(element.dataset.i18nMessage), {
       ALLOWED_TAGS: ["a"],
       ALLOWED_ATTR: ["href"],
     });
+  }
+
+  function createNodes(el, tagConfig) {
+    for (const childEl of [...tagConfig.childNodes]) {
+      if (childEl.nodeName === "#text") {
+        // Ignore whitespace nodes
+        if (!/[^\W]/.test(childEl.textContent)) {
+          continue;
+        }
+        const textOutput = childEl.textContent;
+        el.appendChild(document.createTextNode(textOutput));
+      } else {
+        el.appendChild(createElement(childEl));
+      }
+    }
+    return el;
+  }
+
+  function createElement(tagConfig) {
+    const el = document.createElement(tagConfig.tagName);
+    for (const attr of [...tagConfig.attributes]) {
+      el[attr.name] = attr.value;
+    }
+    let placeholders = [];
+    if (tagConfig.dataset.i18nMessage) {
+      const placeholders = [...createNodes(document.createDocumentFragment(), tagConfig).childNodes].map(e => `${e.outerHTML || e.textContent}`);
+      // Escape to only allow link placeholders, everything is turned to text
+      el.innerHTML = DOMPurify.sanitize(browser.i18n.getMessage(tagConfig.dataset.i18nMessage, placeholders), {
+        ALLOWED_TAGS: ["a"],
+        ALLOWED_ATTR: ["href"],
+      });
+    } else {
+      createNodes(el, tagConfig);
+    }
+    return el;
+  }
+
+  const templates = [...document.querySelectorAll("template")];
+  for (const template of templates) {
+    const output = document.createDocumentFragment();
+    createNodes(output, template.content);
+    template.parentNode.insertBefore(output, template)
   }
 
   const disable = document.getElementById("disable");
